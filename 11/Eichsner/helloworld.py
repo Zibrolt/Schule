d601c1fc8e3d0fef26233d1834b3e3c086704cfc
Zibrolt <alexander@ruda-online.de>
2025-10-22 12:56:04 +0000

    Eichsner22.10

---
+++ 11/Eichsner/helloworld.py
@@ -0,0 +1,14 @@
+a = 5
+b = 6
+print("Hallo Welt!" + "Du bist schön!")
+print(chr('97'))
+print("Das Ergebnis ist: " + a)
+print("Das Ergebnis ist:", a + b)
+print("Das Ergebnis ist:" + ('a' + 'b'))
+print("Das Ergebnis von: ", a, '+', b, " lautet ", (a+b))
+print(ord('A'))
+print("Test \n")
+print("Test" + "\n")
+print("Test", "\n")
+print("Test\nTest".splitlines())
+print("Test" + "\\n")
