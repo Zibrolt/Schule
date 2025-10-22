cffb24c17a7170be12c24e89dd716ac1bc1da845
Zibrolt <alexander@ruda-online.de>
2025-10-22 11:04:25 +0000

    testtttt

--- 11/hundealter.py
+++ 11/hundealter.py
@@ -1,4 +1,12 @@
-name = input("Wie heißt Ihr Hund? ")
-alter = input("Wie alt ist Ihr Hund? ")
-hundealter = int(alter) * 7
-print(f"Der Hund ")
+import math
+def hundealter_berechnen(age):
+    return age * 7
+
+def neu_hundealter_berechnen(age):
+    return 16 * math.log(age, 10) + 31
+
+if __name__ == "__main__":
+    name = input("Wie heißt Ihr Hund: ")
+    alter = int(input("Wie alt ist Ihr Hund: "))
+    hundealter = round(neu_hundealter_berechnen(alter), 2)
+    print(f"Ihr Hund heißt {name} und ist {hundealter} Jahre alt")
