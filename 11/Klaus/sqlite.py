a95a9bf620e996cee1edc3a6de7f8956e89de8f9
Zibrolt <alexander@ruda-online.de>
2026-02-04 13:31:50 +0000

    dys

--- 11/Klaus/sqlite.py
+++ 11/Klaus/sqlite.py
@@ -1,35 +1,84 @@
 import sqlite3
 
-connection = sqlite3.connect("meine_datenbank.db")
-cursor = connection.cursor()
+def init_db():
+    with sqlite3.connect("adressen.db") as conn:
+        cursor = conn.cursor()
+        sql = """
+            CREATE TABLE IF NOT EXISTS adressen (
+            adress_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            firstname TEXT NOT NULL,
+            lastname TEXT NOT NULL,
+            street TEXT NOT NULL,
+            postalcode TEXT NOT NULL,
+            city TEXT NOT NULL,
+            phone TEXT
+            )
+            """
+        cursor.execute(sql)
 
-"""
-sql =   "CREATE TABLE kunde(" \
-        "kdn INTEGER PRIMARY KEY AUTOINCREMENT," \
-        "vorname TEXT," \
-        "nachname TEXT," \
-        "age INTEGER," \
-        "email TEXT" \
-        ")"
-cursor.execute(sql)
+def add_address(fname, lname, street, pc, city, tel):
+    with sqlite3.connect("adressen.db") as conn:
+        cursor = conn.cursor()
+        sql = """
+            INSERT INTO adressen (
+                firstname,
+                lastname,
+                street,
+                postalcode,
+                city,
+                phone
+                )
+                VALUES (?, ?, ?, ?, ?, ?)
+            """
+        
+        if check_adress(fname, lname, street, pc, city, tel):
+            return
+        cursor.execute(sql, (fname, lname, street, pc, city, tel))
 
-sql =   "INSERT INTO kunde VALUES(" \
-        "1000, 'Karl', 'Mayr', 34, 'karl@mayr.de'" \
-        ")"
-cursor.execute(sql)
+def check_adress(fname, lname, street, pc, city, tel):
+    with sqlite3.connect("adressen.db") as conn:
+        cursor = conn.cursor()
+        sql = f"""
+            SELECT 1
+            FROM adressen
+            WHERE firstname = ?
+            AND lastname = ?
+            AND street = ?
+            AND postalcode = ?
+            AND city = ?
+            AND phone = ?
+            """ 
+        res = cursor.execute(sql, (fname, lname, street, pc, city, tel)).fetchone()
+        return res is not None
 
-sql =   "INSERT INTO kunde(vorname, nachname, age, email) VALUES(" \
-        "'Maria', 'Habermann', 55, 'mariah@gmx.de'" \
-        ")"
-cursor.execute(sql)
-connection.commit()
-"""
 
-sql =   "SELECT *" \
-        "FROM kunde"
-result = cursor.execute(sql)
+def remove_address(fname, lname, street, pc, city, tel):
+    with sqlite3.connect("adressen.db") as conn:
+        cursor = conn.cursor()
+        sql = f"""
+            DELETE FROM adressen
+            WHERE firstname = ?
+            AND lastname = ?
+            AND street = ?
+            AND postalcode = ?
+            AND city = ?
+            AND phone = ?
+            """ 
+        cursor.execute(sql, (fname, lname, street, pc, city, tel))
 
+def read_all():
+    with sqlite3.connect("adressen.db") as conn:
+        cursor = conn.cursor()
+        sql = """
+            SELECT * FROM adressen
+            """
+        cursor.execute(sql)
+        ergebnis = cursor.fetchall()
+    return ergebnis
 
-for row in result:
-    print([val for val in row])
-connection.close()
+init_db()
+add_address("Peter", "Mayr", "Auerswaldweg 5", "85058", "Ingolstadt", "")
+add_address("Maria", "Unterforstheimer", "Adlerweg 3", "85118", "Walting", "+491708934788")
+add_address("Anna", "Karenina", "Ostwall 17", "09782", "Potsdam", "03054506767")
+
+print(read_all())
